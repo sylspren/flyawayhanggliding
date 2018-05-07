b8c3a3913edf927425bada68579f348ad5c65453
Lei Huang <lei@goodeggs.com>
2018-05-07 08:14:56 -0700

    read port from env

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
+const PORT = process.env.PORT || 3000;
+
 let app = express();
 
 app.get('/', function (req, res) {
   res.send("I'm alive!");
 });
 
-app.listen(3000);
+app.listen(PORT);
