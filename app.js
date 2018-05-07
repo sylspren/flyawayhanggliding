33682bfce29021b52914a0244f9c188d4d099f73
Lei Huang <lei@goodeggs.com>
2018-05-07 08:20:55 -0700

    add logging and nodemon - dev environment now acceptable

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const PORT = process.env.PORT || 3000;
 
 let app = express();
+
+app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.send("I'm alive!");
