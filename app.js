c952edc275e9289066dc5b49916c5f4ccb86efab
Lei Huang <lei@goodeggs.com>
2018-05-07 08:34:08 -0700

    add existing pages as a starting point

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 
 const PORT = process.env.PORT || 3000;
 
 let app = express();
 
 app.use(morgan('combined'));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send("I'm alive!");
+  res.sendFile(path.join(__dirname, 'web/index.html'));
 });
 
 app.listen(PORT);
