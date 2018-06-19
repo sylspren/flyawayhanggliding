21cbfd31e00d6465e8ede87981c6330bd895bfe7
Lei Huang <lei@goodeggs.com>
2018-06-19 16:51:16 -0700

    port over index to php version

--- php/inc/head.php
+++ php/inc/head.php
@@ -7,25 +7,32 @@
 
     <meta name="description" content="<?php
 if ($description) { echo $description; }
-else { ?>Fly Away Hang Gliding - Learn To Fly!  Individual hang gliding instruction and tandem flights at our training hill and the local mountains in Santa Barbara, California.<?php 
+else { ?>Fly Away Hang Gliding - Learn To Fly!  Individual hang gliding instruction and tandem flights at our training hill and the local mountains in Santa Barbara, California.<?php
 } ?>">
-<meta name="keywords" content="<?php 
-if ($keywords) { echo $keywords; } 
-else { ?>hang gliding, hang gliding santa barbara, santa barbara hang gliding, hang gliding lessons, hang gliding instruction, hang gliding school, hang glider, tandem flight, flight, flying, foot launched, freeflight, soaring, mountain flight, guide service, Santa Barbara, California<?php if ($extra_keywords) { echo ",$extra_keywords"; } ?><?php 
+<meta name="keywords" content="<?php
+if ($keywords) { echo $keywords; }
+else { ?>hang gliding, hang gliding santa barbara, santa barbara hang gliding, hang gliding lessons, hang gliding instruction, hang gliding school, hang glider, tandem flight, flight, flying, foot launched, freeflight, soaring, mountain flight, guide service, Santa Barbara, California<?php if ($extra_keywords) { echo ",$extra_keywords"; } ?><?php
 } ?>">
 
 <?php if (false) { ?>
-    <!-- meta name="description" content="Fly Away Hang Gliding, Hang Gliding Instruction, Hang gliding California, Hang Gliding Santa Barbara, hang gliding lessons, learn hang glide, hang gliding school" --> 
-    <!-- meta name="keywords" content="Santa Barbara, Hang Glide, Hang Gliding, Hang glider, hang, glide, glider, gliding, learn, fly, instruction, sales, service, wing, pilot, aviation, aircraft, flight, footlaunch, freeflight, soar, soaring, thermal, skyfloating, train, trainer, training, school ,instructor, flying, lesson, lessons, launch, teach, teacher, instruction, adventure sport" --> 
- 
+    <!-- meta name="description" content="Fly Away Hang Gliding, Hang Gliding Instruction, Hang gliding California, Hang Gliding Santa Barbara, hang gliding lessons, learn hang glide, hang gliding school" -->
+    <!-- meta name="keywords" content="Santa Barbara, Hang Glide, Hang Gliding, Hang glider, hang, glide, glider, gliding, learn, fly, instruction, sales, service, wing, pilot, aviation, aircraft, flight, footlaunch, freeflight, soar, soaring, thermal, skyfloating, train, trainer, training, school ,instructor, flying, lesson, lessons, launch, teach, teacher, instruction, adventure sport" -->
+
     <!--  key words ....santa barbara hang gliding, hang gliding lessons, hang gliding instruction, hang gliding school, hang glider, tandem flight, flight, flying, foot launched, freeflight, soaring, mountain flight, guide service, Santa Barbara, California -->
 <?php } ?>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/fahg.css" type="text/css">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css" />
     <link rel="stylesheet" href="css/fahg.css" type="text/css">
 
   </head>
 
   <body <?php if ($body_id) { echo "id=\"$body_id\" "; } ?>dir="ltr" lang="en-US">
-
-    <div id="wrapper"> <!-- This centers the entire page -->
 
 <?php include("inc/top_nav.shtml") ?>
--- php/index.php
+++ php/index.php
@@ -1,65 +1,119 @@
-<?php 
+<?php
 $title = "";
 $body_id = "home";
 $description = "Learn To Hang Glide in Santa Barbara!  Individual hang gliding lessons and tandem flights at our training hill and the local mountains in Santa Barbara, California.";
 include("inc/head.php");
 ?>
-<!-- Movie 1 -->
-      <object width="210" height="160">
-<param name="movie" value="http://www.youtube.com/v/Rrtjn4ezBpg&amp;hl=en_US&amp;fs=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/Rrtjn4ezBpg&amp;hl=en_US&amp;fs=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true"
- width="210" height="160"></embed></object>
 
-<!-- Movie 3 -->
-      <object width="210" height="160">
-<param name="movie" value="http://www.youtube.com/v/K2exYzwsQsg?fs=1&amp;hl=en_US"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/K2exYzwsQsg?fs=1&amp;hl=en_US" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" 
-width="210" height="160"></embed></object>
-<!-- Movie 4 -->
-      <object width="210" height="160">
-<param name="movie" value="http://www.youtube.com/v/eWW4VtR3XBY?fs=1&amp;hl=en_US"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/eWW4VtR3XBY?fs=1&amp;hl=en_US" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" 
-width="210" height="160"></embed></object>
+<div class="content">
+  <div class="row">
+    <div class="col-sm-5">
+      <img class="fullwidth" src="fafiles/Willy%20Dydo%20Instructor.jpg" alt="About Willy Dydo Instructor" title="Willy Dydo Instructor"/>
+      <div class="row text-center certifications">
+        <div class="col-lg-6">
+          <a href="https://www.ushpa.org/">
+            <img class="fullwidth" src="fafiles/ushpa_certified.png">
+            <p class="caption">USHPA Certified Instruciton</p>
+          </a>
+        </div>
+        <div class="col-lg-6">
+          <a href="http://www.pasaschools.org/certified-free-flight-schools/">
+            <img class="fullwidth" src="fafiles/pasa_certified.png">
+            <p class="caption">PASA Certified Flight School</p>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 offset-sm-1">
+      <h3>Willy Dydo is your world class instructor.</h3>
+      <p>Certified USHPA Instructor, Willy Dydo, grew up in Vermont.  Ever since his feet left the ground Willy has had a hard time
+ming   down, which is the main reason why he now calls Santa Barbara, with its year-round epic flying, his home.
+ enjoys giving others the gift of flight and helping turn their dreams into reality.
+lly   is one of the best instructors we've ever seen, not only because he's a great pilot, but also because of his
+tience and calm nature. <br clear="all"></p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="sbsa-callout">
+      <a href="http://www.sbsa.info/"><img height="130px" src="fafiles/sbsalogo.png" alt="Santa Barbara Soaring Association"></a>
+      <p>
+        Visit the <a href="http://www.sbsa.info/">Santa Barbara Soaring Association</a> for visiting pilot info and to join our local flying community
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-4 video-container">
+      <a href="https://www.youtube.com/watch?v=Rrtjn4ezBpg&rel=0&autoplay=1" data-toggle="lightbox">
+        <div class="video-thumbnail video-1">
+          <img src="fafiles/yt_icon_mono_light.png">
+        </div>
+      </a>
+    </div>
+    <div class="col-md-4 video-container">
+      <a href="https://www.youtube.com/watch?v=Rrtjn4ezBpg&rel=0&autoplay=1" data-toggle="lightbox">
+      <a href="https://www.youtube.com/watch?v=K2exYzwsQsg&rel=0&autoplay=1" data-toggle="lightbox">
+        <div class="video-thumbnail video-2">
+          <img src="fafiles/yt_icon_mono_light.png">
+        </div>
+      </a>
+    </div>
+    <div class="col-md-4 video-container">
+      <a href="https://www.youtube.com/watch?v=eWW4VtR3XBY&rel=0&autoplay=1" data-toggle="lightbox">
+        <div class="video-thumbnail video-3">
+          <img src="fafiles/yt_icon_mono_light.png">
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-6">
+      <h3>You can soar with the birds. Hang gliding is the ultimate high.</h3>
+      <p>Welcome to Fly Away Hang Gliding. We've been providing hang gliding instruction and tandems to help people realize their dreams of free flight since 1995. We are located in beautiful Santa Barbara, California.</p>
 
-
-      <p>Welcome to Fly Away Hang Gliding. We've been providing hang gliding instruction and tandems to help people realize their dreams of free flight since 1995.  We are located in beautiful Santa Barbara, California.</p>
-
-      <p>Learn to fly hang gliders on our 200 foot high training hill. It is one of the easiest-to-learn-on training hills in the world.  It's warm and 
-      sunny here in Santa Barbara year round, and we have fresh, clean, smog-free air.  The weather is great for hang gliding lessons year round.</p>
+      <p>Learn to fly hang gliders on our 200 foot high training hill. It is one of the easiest-to-learn-on training hills in the world. It's warm and sunny here in Santa Barbara providing for perfect year round flying conditions.  Weather you are local or looking to travel to obtain your H2 novice rating, Fly Away Hang Gliding is your premier location.</p>
 
       <p>Hang gliding is the closest humans can get to the feeling of flying like a bird, like in your dreams.</p>
-
-      <p>You can soar with the birds. Hang gliding is the ultimate high.</p>
-
-      <img align="left"src="fafiles/AnaiyaMain.jpg" width="315" height="240" border="0" name="Ground Run Aplpa" alt="Ground Run Bottom Alpha" title="Running Alpha">
-
-      <img align="right"src="fafiles/topalpha.jpeg" width="315" height="240" border="0" name="upper launch" alt="upper launch alpha" title="Launching The Top"> 
-
-      <span class="side1">
-        We are available 7 days a week, by appointment.  Our classes often fill up during the weekend so call ahead for class schedules.  Lesson vouchers are available for single or multiple day classes.<br><br><br>
-      </span>
-
+    </div>
+    <div class="col-sm-5 offset-sm-1">
+      <img class="fullwidth" src="fafiles/AnaiyaMain.jpg" alt="Ground Run Bottom Alpha" title="Running Alpha">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-5">
+      <img class="fullwidth" src="fafiles/topalpha.jpeg" alt="upper launch alpha" title="Launching The Top">
+    </div>
+    <div class="col-sm-6 offset-sm-1">
+      <p>
+        We are available 7 days a week, by appointment.  Our classes often fill up during the weekend so call ahead for class schedules.  Lesson vouchers are available for single or multiple day classes.
+      </p>
       <p>
         Hang gliding lessons are $200 per person per day.<br>
         Guaranteed private lessons are $300.<br>
-        
         H2 Novice Rating Course Package $1,500<br>
         Single Day Lessons are credited towards Package<br>
         We don't accept credit cards.<br>
-        <br clear="all">
-        </p>
-
-      <p><img src="fafiles/Park Map.JPG" id="thill_map" style="float: right" border="0" height="292" width="360" alt="Map to elings park" title="map of training hill"></p>
-
-      <p>Directions to Elings Park.<br>
-        Google <a href="http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=2650+Cliff+Drive,+Santa+Barbara,+CA&sll=34.405325,-119.736321&sspn=0.006117,0.004903&gl=us&ie=UTF8&hq=&hnear=2650+Cliff+Dr,+Santa+Barbara,+California+93101&ll=34.405024,-119.737383&spn=0.006117,0.008991&z=17">2650 Cliff Dr., Santa Barbara</a>. There is no address there, but it will put you very close to the entrance.<br>
       </p>
-
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+      <h4>Directions to Elings Park.</h4>
+      <p>
+        Google <a href="http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=2650+Cliff+Drive,+Santa+Barbara,+CA&sll=34.405325,-119.736321&sspn=0.006117,0.004903&gl=us&ie=UTF8&hq=&hnear=2650+Cliff+Dr,+Santa+Barbara,+California+93101&ll=34.405024,-119.737383&spn=0.006117,0.008991&z=17">2650 Cliff Dr., Santa Barbara</a>. There is no address there, but it will put you very close to the entrance.
+      </p>
       <p>
         If you are traveling north on Hyw. 101 from Los Angeles, you will go left on Las Positas, towards the ocean. If you are traveling south from San Fransisco, go right on Las Positas.
-        <br>
         <br>
         Go about 1 mile, through several stoplights and you will come to a "T" intersection at a stop sign with a flashing red light. <br>
         This is Cliff Dr.  Take a left. <br>
         COUNT 6 TELEPHONE POLES (a couple hundred yards) on the left side and there will be a blacktop driveway. Turn in there. <br>
         Go up the driveway and stay to the left. The road will turn to dirt and go through a second gate.  Park by the plastic porta-potty. <br>
        </p>
+    </div>
+    <div class="col-md-6">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3291.855286232322!2d-119.737383!3d34.405024!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e9144e136d8e03%3A0xf2b6e13c04b27c9c!2s2650+Cliff+Dr%2C+Santa+Barbara%2C+CA+93101!5e0!3m2!1sen!2sus!4v1525719083988" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+  </div>
+</div>
 
 <?php include("inc/foot.php") ?>
