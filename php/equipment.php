2fa61d065c44934d61fa1c694a2a65648c5d1bc0
Lei Huang <lei@goodeggs.com>
2018-06-19 18:54:34 -0700

    bring other pages in line with style

--- php/equipment.php
+++ php/equipment.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $title = "Equipment";
 $body_id = "equipment";
 $description = "Fly Away Hanggliding can help you find new or used equipment.";
@@ -8,40 +8,38 @@ $description = "Fly Away Hanggliding can help you find new or used equipment.";
 
 include("inc/head.php");
 ?>
+<div class="content equipment">
+  <h4>We sell everything you need - Beginner to Competition level Gliders, Harnesses, Parachutes, Helmets, and Flight instruments.</h4>
+  <p> Hang Gliding equipment is constantly evolving and improving.  The handling, performance, and landing characteristics of modern hang gliders is unbelievable.   For the greatest satisfaction and safety, new equipment is highly recommended and well worth it.  </p>
+  <div class="text-center">
+  <a href="http://willswing.com/prod1.asp?theClass=hg" name="wwhanggliders" id="wwhanggliders"><img src="fafiles/ww_logo.gif" width="246" height="53" border="0" alt="willswing hang gliders"> <br>Wills Wing Hang Gliders</a>
+  <br clear="all"><br>
 
-<a href="http://willswing.com/prod1.asp?theClass=hg" name="wwhanggliders" id="wwhanggliders"><img src="fafiles/ww_logo.gif" width="246" height="53" border="0" alt="willswing hang gliders"> <br>Wills Wing Hang Gliders</a>
-<br clear="all"><br>
-
-<a href="http://www.northwing.com/freedom-hang-glider.htm" name="northwing" id="northwing"><img src="fafiles/northwing_logo.jpg" width="370" height="70" border="0" name="northwing_logo" alt="northwing_logo" align="middle"> <br> North Wing Hang Gliders</a>
-<br clear="all"><br>
+  <a href="http://www.northwing.com/freedom-hang-glider.htm" name="northwing" id="northwing"><img src="fafiles/northwing_logo.jpg" width="370" height="70" border="0" name="northwing_logo" alt="northwing_logo" align="middle"> <br> North Wing Hang Gliders</a>
+  <br clear="all"><br>
 
 
-<a href="http://www.moyes.com.au/products.asp" name="moyes" id="moyes"><img src="fafiles/moyes_logo.jpg" width="366" height="88" border="0"alt="moyes hang gliders" align="middle"> <br>Moyes Hang Gliders</a>
-<br clear="all"><br>
+  <a href="http://www.moyes.com.au/products.asp" name="moyes" id="moyes"><img src="fafiles/moyes_logo.jpg" width="366" height="88" border="0"alt="moyes hang gliders" align="middle"> <br>Moyes Hang Gliders</a>
+  <br clear="all"><br>
 
-<a href="http://www.highenergysports.com/" name="high_energy" id="high_energy"> <img src="fafiles/high_energy_logo.gif" width="370" height="61" border="0" alt="High Energy harnesses" align="middle"> <br>High Energy Harnesses</a>
-<br>
-<br clear="all">
+  <a href="http://www.highenergysports.com/" name="high_energy" id="high_energy"> <img src="fafiles/high_energy_logo.gif" width="370" height="61" border="0" alt="High Energy harnesses" align="middle"> <br>High Energy Harnesses</a>
+  <br>
+  <br clear="all">
 
-<a href="http://www.flytec.com/index.htm" name="flytec" id="flytec"> <img src="fafiles/flyteclogo.png" width="370" height="61" border="0" alt="Flytec varios" align="middle"> <br>Flytec Varios</a>
-<br clear="all">
+  <a href="http://www.flytec.com/index.htm" name="flytec" id="flytec"> <img src="fafiles/flyteclogo.png" width="370" height="61" border="0" alt="Flytec varios" align="middle"> <br>Flytec Varios</a>
+  <br clear="all">
 
-<a href="http://www.digifly.com/index.htm" name="digifly" id="digifly"> <img src="fafiles/digiflylogo.jpg" width="320" height="124" border="0" alt="Flytec varios" align="middle"> <br>DigiFly Varios</a>
+  <a href="http://www.digifly.com/index.htm" name="digifly" id="digifly"> <img src="fafiles/digiflylogo.jpg" width="320" height="124" border="0" alt="Flytec varios" align="middle"> <br>DigiFly Varios</a>
+  </div>
 
-<H3> We sell new and used hang gliding equipment.  We also purchase used equipment.</H3>
-
-<span class="side1">New Hang Gliding Equipment<br>
-$3400 to $4000 Novice hang glider.<br>
-$450 to $1200  Parachute<br>
-$500 to $1400  Harness<br>
-$220  Helmet
-</span>
-
-<p>Used equipment<br>
-$1500 Novice Hang glider <br>
-$100 to $600 Parachute <br>
-$100 to $700 Harness <br>
-$25 to $100  Helmet</P>
-
+  <p>New Hang Gliding Equipment</p>
+  <ul>
+    <li>$3400 to $4000 Novice hang glider.</li>
+    <li>$450 to $1200  Parachute</li>
+    <li>$500 to $1400  Harness</li>
+    <li>$220  Helmet</li>
+  </ul>
+  </span>
+</div>
 
 <?php include("inc/foot.php") ?>
